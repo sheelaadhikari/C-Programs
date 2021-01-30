c3f9007128e47218ce5921dee06a40f90b8ce4fb
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-01-30 17:36:57 +0545

    january

---
+++ january.c
@@ -0,0 +1,21 @@
+// program to find factorial of any number
+#include <stdio.h>
+void main()
+{
+    int n, num;
+    long fact = 1;
+    printf("enter the number: ");
+    scanf("%d", &n);
+    num = n;
+    if (n < 0)
+        printf("no factoril of negative number\n");
+    else
+    {
+        while (n > 1)
+        {
+            fact *= n;
+            n--;
+        }
+        printf("factoril of %d=%d\n", num, fact);
+    }
+}
