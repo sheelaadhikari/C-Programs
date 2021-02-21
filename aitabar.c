60af0dd5ea981d926bd41ba7a543f25ae0b96e40
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-02-21 17:05:57 +0545

    maximum using array

---
+++ aitabar.c
@@ -0,0 +1,16 @@
+// wap  to find the greateat number among five.
+#include <stdio.h>
+int main()
+{
+    int i, a[5], max = 0;
+    for (i = 0; i <= 5; i++)
+    {
+        printf("enter the number: ");
+        scanf("%d", a[i]);
+
+        if (a[i] > max)
+            max = a[i];
+    }
+    printf("the maximum number is %d", a[i]);
+    return 0;
+}
