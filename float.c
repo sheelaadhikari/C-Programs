47460b901d967017173d3f91d6316d77199e404e
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-02-21 17:37:30 +0545

    float

---
+++ float.c
@@ -0,0 +1,8 @@
+// program to understand the use of floating point arithematic operation
+#include <stdio.h>
+void main(){
+    float a=12.4, b=3.8;
+    printf("sum=%.2f\n",a+b);
+    printf("difference=%.2f\n", a-b);
+    printf("product=%.2f", a*b);
+}
