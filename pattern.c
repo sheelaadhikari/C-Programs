56945a0d901941d15d91fa549890bdea1b32b44f
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-02-22 17:16:05 +0545

    star pattern

---
+++ pattern.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main(){
+    int i, j, n;
+    printf("enter the number: ");
+    scanf("%d", &n);
+    for(i=1; i<=4; i++){
+        for(j=1; j<=i; j++){
+            printf("*");
+
+        }
+        printf("\n");
+        
+    }
+}
