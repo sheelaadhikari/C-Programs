a63374aae8ebdb1a7875d3f52d299daed74a23da
Sheela Adhikari <adhikarisheela1000@gmail.com>
2021-03-03 14:28:17 +0545

    addition in array

---
+++ addition.c
@@ -0,0 +1,32 @@
+//wap to add the matrix form of array elements.
+#include <stdio.h>
+void main(){
+    int i, j, num[2][2], arr[2][2];
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("enter any value: ");
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("enter any value: ");
+            scanf("%d", &num[i][j]);
+
+
+        }
+    }
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            num[i][j]=num[i][j]+arr[i][j];
+
+        }
+    }
+    for(i=0; i<2; i++){
+        for(j=0; j<2; j++){
+            printf("%d\t", num[i][j]);
+        }
+        printf("\n");
+    }
+}
+
